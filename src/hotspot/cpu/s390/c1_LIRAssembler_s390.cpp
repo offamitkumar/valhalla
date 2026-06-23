5051a865947c1f711bfaf0858695f4f090ca40d7
Amit Kumar <amitlocham09@icloud.com>
2026-06-23 08:27:59 +0000

    fix

--- src/hotspot/cpu/s390/c1_LIRAssembler_s390.cpp
+++ src/hotspot/cpu/s390/c1_LIRAssembler_s390.cpp
@@ -3078,7 +3078,7 @@ void LIR_Assembler::emit_profile_inline_type(LIR_OpProfileInlineType* op) {
   // OR with flag
   __ z_oill(Z_R0_scratch, flag);
   // Store back
-  __ z_stc(Z_R0_scratch, Address(mdo_base, mdo_offs));
+  __ z_stcy(Z_R0_scratch, Address(mdo_base, mdo_offs));
 
   __ bind(not_inline_type);
 }
